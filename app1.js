c4601c0b43f9d652c217764f58e54f4bb2f6b359
Ezzytechs <jameze49@gmail.com>
2025-11-11 18:29:36 +0100

    intro to file structure

--- app1.js
+++ app1.js
@@ -1,4 +1,10 @@
-console.log("This is app 1....works fine");
-document.getElementById("main").innerHTML="Helllo Saturday";
-console.log("New Update")
-console.log("New Update2")
+const element = document.getElementById("main");
+let nameL = element.innerHTML;
+// if(nameL==="Hello World")
+if (nameL.length > 7) {
+  element.style.color = "green";
+} else {
+  nameL = "Helllo";
+  element.style.color = "red";
+}
+console.log(nameL);
--- folder2/app2.js
+++ folder2/app2.js
@@ -1,1 +1,4 @@
 console.log("This is app 2");
+console.log("This is app 2");
+// git remote -v                # List remotes
+// git remote add origin <url> 
